22e7ca2f6ef43285d164aec5331e895ffd3d348b
Luca Armaroli <lukaarma@gmail.com>
2020-09-16 19:23:25 +0200

    directory listing is now iterative

--- src/FileLister.java
+++ src/FileLister.java
@@ -4,20 +4,20 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class FileLister {
-    private final Path rootPath;
     private final ArrayList<Path> fileList;
 
 
     public FileLister(String rootPath) throws EmptyFolderException {
-        this.rootPath = Path.of(rootPath);
         this.fileList = new ArrayList<>();
-        listFiles(this.rootPath);
+        listFiles(Path.of(rootPath));
 
         if (this.fileList.isEmpty()) {
-            throw new EmptyFolderException();
+            throw new EmptyFolderException(rootPath);
         }
     }
 
@@ -26,42 +26,39 @@ public class FileLister {
         return fileList;
     }
 
-    public Path getRootPath() {
-        return rootPath;
-    }
 
-    // FIXME: make this with iteration not recursive!!
-    // I think this may overflow the jvm memory at some point
-    // but the directory has to be so big that for now it will work
     private void listFiles(Path rootPath) {
-        DirectoryStream<Path> files;
-        try {
-            files = Files.newDirectoryStream(rootPath);
-        }
-        catch (IOException e) {
-            System.out.printf("Error while opening '%s' as a directory for reading, skipping... \n",
-                              rootPath);
-            return;
-        }
+        ArrayList<Path> folders = new ArrayList<>(List.of(rootPath));
+        AtomicReference<Integer> nextIndex = new AtomicReference<>(folders.size());
 
-        files.forEach(path -> {
-            if (Files.isDirectory(path)) {
-                listFiles(path);
+        for (int i = 0; i < folders.size(); i++) {
+            DirectoryStream<Path> folderContent;
+            try {
+                folderContent = Files.newDirectoryStream(folders.get(i));
             }
-            else if (Files.isReadable(path) && Files.isRegularFile(path)) {
-                this.fileList.add(path);
-            }
-            else {
-                System.out.printf("Error while parsing '%s': it's not a directory nor a regular file, skipping \n...",
-                                  path);
-            }
-        });
+            catch (IOException e) {
+                System.out.printf(
+                        "Error while opening '%s' as a directory for reading, skipping... \n",
+                        folders.get(i)
+                );
 
-//        return Files.walk(this.rootPath)
-//                .filter(Files::isRegularFile)
-//                .filter(Files::isReadable)
-//                .toArray(Path[]::new);
+                continue;
+            }
+
+            folderContent.forEach(path -> {
+                if (Files.isDirectory(path)) {
+                    folders.add(nextIndex.getAndSet(nextIndex.get() + 1), path);
+                }
+                else if (Files.isReadable(path) && Files.isRegularFile(path)) {
+                    this.fileList.add(path);
+                }
+                else {
+                    System.out.printf(
+                            "Error while parsing '%s': it's not a directory nor a regular/readable file, skipping \n...",
+                            path
+                    );
+                }
+            });
+        }
     }
-
-
 }
